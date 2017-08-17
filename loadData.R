ab2e452f15469d18c7d7bc96a344bac83a9d5e40
apratap <abhishek.vit@gmail.com>
2017-08-17 10:37:04 -0700

    latest changes

--- loadData.R
+++ loadData.R
@@ -11,6 +11,7 @@ synapseLogin()
 
 
 #1. Get data
+#passive data
 passive_data <- fread(synGet("syn10236538")@filePath, data.table = F) %>% 
   dplyr::mutate(day = as.numeric(day),
                 passive_date_pacific = as.Date(passive_date_pacific),
@@ -18,18 +19,27 @@ passive_data <- fread(synGet("syn10236538")@filePath, data.table = F) %>%
   dplyr::filter(!study_arm %in% c(NA, '')) %>%
   select(-brightenid, -Cohort, -User_Phone_Type, -study_arm) %>% as.data.frame()
 
+#PHQ2
 phq2  <- fread(synGet("syn10236539")@filePath, data.table = F) 
 phq2 <- phq2 %>% 
   dplyr::mutate(phq2_date_local = as.Date(phq2_date_local),
                 start = as.Date(start),
+                week = ((day - 1) %/% 7) + 1 ,
                 user_id = as.character(user_id)) %>%
   filter(!study_arm %in% c(NA, '')) %>%
   dplyr::select(-brightenid, -study_arm) 
-
 #summarize more than one phq2 recording in a day
 phq2 <- phq2 %>% group_by(user_id, day, start) %>% 
   summarise_all(.funs=function(x) mean(x, na.rm=T)) %>% as.data.frame()
 
+
+#PHQ9
+phq9  <- fread(synGet("syn10236540")@filePath, data.table = F) 
+phq9 <- phq9 %>% dplyr::mutate(start = as.Date(start),user_id = as.character(user_id)) %>%
+  dplyr::select(-brightenid) 
+
+#metadata
+metaData  <- fread(synGet("syn10236547")@filePath, data.table = F) 
 
 #2. MERGE - Passive features and PHQ2 (daily mood)
 # x <- passive_data %>% filter(user_id == '15919') %>% mutate(start = as.character(start))
@@ -49,6 +59,9 @@ phq2 <- phq2 %>% group_by(user_id, day, start) %>%
 tmp_phq2 <- phq2 %>%  mutate(start = as.character(start), day = day-1)
 tmp_passive_data <- passive_data %>% mutate(start = as.character(start))
 intersect(colnames(tmp_passive_data), colnames(tmp_phq2))
+# str(tmp_passive_data)
+# str(tmp_phq2)
+### Merged Passive and PHQ2 data
 passive_n_phq2 <- merge(tmp_passive_data, tmp_phq2, all.x=T, all.y=T)
 
 #######################
@@ -61,18 +74,16 @@ tmp_impute_col <- function(col){
 }
 sum(complete.cases(passive_n_phq2))
 
-str(passive_n_phq2)
 # Step 1 - fill missing values based on the values in that week
 passive_n_phq2_with_imputed_vals <- passive_n_phq2  %>% group_by(user_id, week, start) %>%
   mutate_all(.funs = tmp_impute_col )
 sum(complete.cases(passive_n_phq2_with_imputed_vals))
 
 
-x1 <- phq2  %>% dplyr::group_by(user_id) %>% dplyr::summarise(phq2_count = n())
-x2 <- passive_data  %>% dplyr::group_by(user_id) %>% dplyr::summarise(passive_count = n())
-x <- merge(x1,x2)
-p1 <- ggplot(data=x, aes(x=phq2_count, y=passive_count)) + geom_point(size=.6) + scale_color_ptol("cyl") + theme_minimal()
-p1
+# x1 <- phq2  %>% dplyr::group_by(user_id) %>% dplyr::summarise(phq2_count = n())
+# x2 <- passive_data  %>% dplyr::group_by(user_id) %>% dplyr::summarise(passive_count = n())
+# x <- merge(x1,x2)
+# p1 <- ggplot(data=x, aes(x=phq2_count, y=passive_count)) + geom_point(size=.6) + scale_color_ptol("cyl") + theme_minimal()
 
 missingNess <- function(vec){
   missingVals <- sum(is.na(vec)) + sum(vec == '', na.rm = T)
@@ -87,7 +98,6 @@ missingData <- passive_n_phq2 %>% dplyr::select(c(3:13,17), user_id, sum_phq2) %
 #Users who have atleast 5 features with less than 40% data missing
 keep_users <- missingData %>% select(-user_id) %>% apply(1, function(x) sum(x > 40))  < 5
 SELECTED_USERS <- missingData$user_id[keep_users]
-SELECTED_USERS
 passive_n_phq2 <- passive_n_phq2 %>% filter(user_id %in% SELECTED_USERS)
 
 
@@ -121,5 +131,5 @@ FINAL_DATA <- passive_n_phq2_with_imputed_vals[complete.cases(passive_n_phq2_wit
 mdata <- fread(synGet("syn10236547")@filePath, data.table=F)
 FINAL_DATA <-  merge(FINAL_DATA, mdata, all.x=T)
 
-rm(keep_users, missingData, missingNess, p1, passive_n_phq2, passive_n_phq2_with_imputed_vals,
-  SELECTED_USERS, tmp_impute_col, tmp_passive_data, tmp_phq2, x, x1, x2)
+rm(keep_users, missingData, missingNess, passive_n_phq2_with_imputed_vals,
+  SELECTED_USERS, tmp_impute_col, tmp_passive_data, tmp_phq2)
