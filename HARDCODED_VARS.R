e3fc743c546536d11f988f56facfbf5f5e55b4b0
apratap <abhishek.vit@gmail.com>
2017-08-25 05:26:22 +0000

    file to store GLOBAL vars

---
+++ HARDCODED_VARS.R
@@ -0,0 +1,9 @@
+#Hardcoded PASSIVE Features
+PASSIVE_COL_NAMES <- c('unreturned_calls', 'mobility', 'sms_length', 'call_duration',
+                       'interaction_diversity', 'missed_interactions' ,'aggregate_communication',
+                       'sms_count', 'mobility_radius', 'call_count')
+
+
+
+
+
